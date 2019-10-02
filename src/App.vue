c17909e22e7ea6eab5a9561bd550ed8429400e7c
felixliao <felixliao@micb2b.com>
2019-10-02 11:28:05 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <component :is="layout">
+      <router-view></router-view>
+    </component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const default_layout = "default"
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  computed: {
+    layout(){
+      return default_layout + "-layout";
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/css/simple-sidebar.css';
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+import 'jquery';        // 載入 jQuery
+import 'bootstrap/dist/css/bootstrap.css'; // 載入 bootstrap CSS 檔
+import 'bootstrap';      // 載入 bootstrap 的 JS 檔
+import PortalVue from 'portal-vue';
+import BootstrapVue from 'bootstrap-vue';
+
+window.$ = require('jquery');
+window.JQuery = require('jquery');
+Vue.use(BootstrapVue);
+Vue.use(PortalVue);
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // fas: 全部icon
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(fas);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+import Default from './layouts/Default.vue';
+
+Vue.component('default-layout', Default);
+
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
